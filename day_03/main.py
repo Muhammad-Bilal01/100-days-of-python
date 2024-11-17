04fd319142e95ced3d4280bc08ef05496a88ed2f
Muhammad Bilal <mbilal254.m@gmail.com>
2024-11-18 01:46:07 +0500

    day 03

---
+++ day_03/main.py
@@ -0,0 +1,11 @@
+# import pandas
+import random
+
+# Read and work with a file named 'words.csv'
+
+# df = pandas.read_csv('words.csv')
+# print(df) # This will display first few rows from the words.csv files
+
+ran = random.randint(0,20)
+
+print(ran)
