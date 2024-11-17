a55c06597fed16a912d8560904c719834d49979c
Muhammad Bilal <mbilal254.m@gmail.com>
2024-11-18 01:54:02 +0500

    day 04

---
+++ day_o4/main.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print(5)
+print(17*21)
+print('BYE')
+
+# Quiz #1
+print("""
+    Citron, pomegranate,
+     Apricot, and peach,
+  Flutter of apple-blows
+     Whiter than the snow,
+  Filling the silence
+     With their leafy speech,
+  Budding and blooming
+     Down row after row.""")
