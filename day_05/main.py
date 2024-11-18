87ecb0e5c99f4d5a7ff4397a8d9308fec9acfb20
Muhammad Bilal <mbilal254.m@gmail.com>
2024-11-18 13:18:56 +0500

    day 05

---
+++ day_05/main.py
@@ -0,0 +1,19 @@
+print("Hello")
+
+# single line comment
+
+# Multi
+# line 
+# comment
+
+"""
+This is also a comment
+"""
+
+# Escape sequence
+print("this is /n muhammad bilal")
+
+print("Hello","world",sep="-")
+
+print("change newline",end="*")
+print("checkout",end="*")
